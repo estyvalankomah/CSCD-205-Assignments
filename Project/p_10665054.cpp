d5830129db3ebcdb8361f752bd9fe15aa0160301
estyvalankomah <abenammra2002@gmail.com>
2018-12-18 00:02:17 -0800

    made some changes

--- Project/p_10665054.cpp
+++ Project/p_10665054.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include "library/Database.h"
 #include <string>
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  
+  User one;
+  cout << "Enter your ID number : ";
+  cin >> one.user_number;
+  cout << "Enter your pin : ";
+  cin >> one.user_pin;
+  cout << "Enter your first name : ";
+  cin >> one.fname;
+  cout << "Enter your last name : ";
+  cin >> one.lname;
+  cout << "Enter user type (admin/staff/student) : ";
+  cin >> one.user_type;
+
   return 0;
 }
