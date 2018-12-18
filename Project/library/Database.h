536ecca9381f0aa084afa1d708f3c425cb75eb4b
unknown <ebaidoo79@gmail.com>
2018-12-18 02:05:36 +0000

    I have completed my work

--- Project/library/Database.h
+++ Project/library/Database.h
@@ -2,8 +2,10 @@
 #define DATABASE_H
 //this is where all includes will be
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 
 //this is where all defined constants will be
 #define FILE_NAME "file.txt"
@@ -18,28 +20,29 @@ what we are going to have create, read, edit and delete functions
 
 //this is your defined user struct
 struct User{
-  string user_number;
-  string user_pin;
-  string fname;
-  string lname;
-  string user_type;
+  std::string user_number;
+  std::string user_pin;
+  std::string fname;
+  std::string lname;
+  std::string user_type;
 };
 
 class Database{
-  fstream file;
-  fstream temp_file;
+  std::fstream file;
+  std::fstream temp_file;
+  User user;
 public:
 
-  //create methods
+  //create methods (works as planned)
   void create(User& user){
     //lines to write to file
     file.open("users.txt",std::ios::app|std::ios::out);
-    file << user.user_number << " " << user.user_pin << " " << user.fname << " " << user.lname << " " << user.user_type << endl;
+    file << user.user_number << " " << user.user_pin << " " << user.fname << " " << user.lname << " " << user.user_type << std::endl;
     file.close();
     std::cout << "User created successfully " << std::endl;
   }
 
-  //fetch(read) methods
+  //fetch(read) methods (works as planned)
   void fetch(std::string user_id,User& user){
     //lines to fetch the user from the file
     file.open("users.txt",std::ios::in);
@@ -51,41 +54,68 @@ public:
     }
   }
 
-  //update(edit) methods
+  //update(edit) methods (not working at the moment) why are you cursing lol am I ? you said wtf that's cursing lol
   bool update(User update_user){
-    User user;
     //lines to update the user in the file
-    file.open("users.txt",std::ios::in|std::ios::out);
-    temp_file.open("temp.txt",std::ios::in|std::ios::out);
+    file.open("users.txt");
+    temp_file.open("temp.txt",std::ios::app|std::ios::out);
     while(!file.eof()){
       file >> user.user_number >> user.user_pin >> user.fname >> user.lname >> user.user_type;
+      // std::cout << "Fetched : " << user.user_number << " With id : " << user.user_number << std::endl;
       if(user.user_number == update_user.user_number){
-        user.user_number = upadte_user.user_number;
-        user.user_pin = upadte_user.user_pin;
-        user.fname = upadte_user.fname;
-        user.lname = upadte_user.lname;
-        user.user_type = upadte_user.user_type;
+        user.user_number = update_user.user_number;
+        user.user_pin = update_user.user_pin;
+        user.fname = update_user.fname;
+        user.lname = update_user.lname;
+        user.user_type = update_user.user_type;
       }
-      temp_file << user.user_number << " " << user.user_pin << " " << user.fname << " " << user.lname << " " << user.user_type << endl;
+      temp_file << user.user_number << " " << user.user_pin << " " << user.fname << " " << user.lname << " " << user.user_type << std::endl;
     }
     file.close();
     temp_file.close();
     remove("users.txt");
-    if(rename("temp.txt","users.txt")){
+    int check = rename("temp.txt","users.txt");
+    std::cout << check << std::endl;
+    if(check){
       return true;
     }
     return false;
   }
 
-
   //delete(remove user) methods
-  void delete_user(string user_id){
+  void delete_user(std::string user_id){
     //lines to delete the user
+    file.open("users.txt");
+    temp_file.open("temp.txt",std::ios::app|std::ios::out);
+    while(!file.eof()){
+      file >> user.user_number >> user.user_pin >> user.fname >> user.lname >> user.user_type;
+      // std::cout << "Fetched : " << user.user_number << " With id : " << user.user_number << std::endl;
+      if(user.user_number == user_id){
+        //if it passes for the test it will skip the rest if the lines in the loop
+        //hence not writing the user we want to delete to the file
+        continue;
+      }
+      temp_file << user.user_number << " " << user.user_pin << " " << user.fname << " " << user.lname << " " << user.user_type << std::endl;
+    }
+    file.close();
+    temp_file.close();
+    remove("users.txt");
+    int check = rename("temp.txt","users.txt");
+    std::cout << check << std::endl;
+    if(check){
+      return true;
+    }
+    return false;
   }
 
   //user authentication method
-  bool login(string user_id,string user_pin){
+  bool login(std::string user_id,std::string user_pin){
     //line to log user into his/her account
+    User user;
+    this->fetch(user_id,user);
+    if(user.pin == user_pin){
+      return true;
+    }
     return false;
   }
 
--- Project/p_10665054.cpp
+++ Project/p_10665054.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-#include "library/Database.h"
+#include "library\Database.h"
 #include <string>
 using namespace std;
 
+User one;
+//Can you here the voice? no
+//okay so lets do some fetching from the database with a particular id okay
+//are you  I'm here yes yeah yes start all over
+//okay so it worked as planned yeah whtever lol
 int main(int argc, char const *argv[]) {
-  User one;
+  Database database;
+  string id = "1111";
   cout << "Enter your ID number : ";
   cin >> one.user_number;
   cout << "Enter your pin : ";
@@ -15,6 +21,9 @@ int main(int argc, char const *argv[]) {
   cin >> one.lname;
   cout << "Enter user type (admin/staff/student) : ";
   cin >> one.user_type;
-
+  // database.create(one);//Esther fill the form
+  if(database.update(one)){
+      cout << " User was updated successfully" << endl;
+  }
   return 0;
 }
