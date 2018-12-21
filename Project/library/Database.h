33f09e00ecf6a2faca5aa00357a7a87a3f884f2f
estyvalankomah <abenammra2002@gmail.com>
2018-12-21 01:46:37 -0800

    Final changes

--- Project/library/Database.h
+++ Project/library/Database.h
@@ -1,18 +1,13 @@
 #ifndef DATABASE_H
 #define DATABASE_H
-//this is where all includes will be
+
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-/*
-okay so this is the main database class alright,
-what we are going to have create, read, edit and delete functions
 
-*/
 
-//this is your defined user struct
 struct User{
   std::string user_number;
   std::string user_pin;
@@ -35,11 +30,11 @@ struct Course{
 class Database{
   std::fstream file;
   std::fstream temp_file;
+  std::fstream course_file;
   User user;
   Course course;
 public:
-
-  //create methods (works as planned)
+  //method to create a user
   void create(User& user){
     //lines to write to file
     file.open("files/users.txt",std::ios::app|std::ios::out);
@@ -47,32 +42,55 @@ public:
     file.close();
     std::cout << "User created successfully " << std::endl;
   }
-
-  //nice implementation here //good work done 
+  //User calls this method to add course(s) and creates a course file containing the ID numbers of all students taking that particular course
   void create(Course& course){
     course.file_name = "files/studentfiles/" + course.user_number + ".txt";
+    std::string course_name = "files/courses/" + course.course_code + ".txt";
     file.open(course.file_name.c_str(),std::ios::app|std::ios::out);
-    //std::cout << course.course_code << " " << course.course_title  << " " << course.credit << " " << course.grade << " " << course.gradept << std::endl;
+    course_file.open(course_name.c_str(),std::ios::app|std::ios::out);
     file << course.course_code << " " << course.course_title  << " " << course.credit << " " << course.grade << " " << course.gradept << std::endl;
+    course_file << course.user_number;
     file.close();
+    course_file.close();
     std::cout << "Course created successfully .." << std::endl;
   }
 
-  //okay so view courses works fine
-  int view(std::string user_id){
+  void view_users(){
+    std::string user ;
+    file.open("files/users.txt",std::ios::in);
+    while(!file.eof()){
+      getline(file,user);
+      std::cout << user << std::endl;
+    }
+    file.close();
+  }
+
+  int view_course(std::string user_id){
     std::string filename = "files/studentfiles/" + user_id + ".txt";
-    file.open(filename,std::ios::in);
+    file.open(filename.c_str(),std::ios::in);
     if(!file.is_open()){
       return 1;
     }
     while(!file.eof()){
       getline(file,filename);
-      //pardon me I am feeling lazy to create a new variable so I am killing it all here
       std::cout << filename << std::endl;
     }
+    file.close();
   }
 
-  //fetch(read) methods (works as planned)
+  int view_student(std::string course_code){
+    std::string filename = "files/courses/" + course_code + ".txt";
+    file.open(filename.c_str(),std::ios::in);
+    if(!file.is_open()){
+      return 1;
+    }
+    while(!file.eof()){
+      getline(file,filename);
+      std::cout << filename << std::endl;
+    }
+    file.close();
+  }
+
   void fetch(std::string user_id,User& user){
     //lines to fetch the user from the file
     file.open("files/users.txt",std::ios::in);
@@ -85,27 +103,14 @@ public:
     file.close();
   }
 
-  void fetch(std::string course_code,Course& update_course){
-    update_course.file_name = "files/studentfiles/" + update_course.user_number + ".txt";
-    file.open(update_course.file_name.c_str(),std::ios::in);
-    while(!file.eof()){
-      file >> course.course_code >> course.course_title >> course.credit >> course.grade >> course.gradept;
-      if(course.course_code == course_code){
-        break;
-      }
-    }
-    file.close();
-  }
 
-  //update(edit) methods (not working at the moment) why are you cursing lol am I ? you said wtf that's cursing lol
-  bool update(User update_user){
+  bool update(std::string user_id,User update_user){
     //lines to update the user in the file
     file.open("files/users.txt");
     temp_file.open("files/temp.txt",std::ios::app|std::ios::out);
     while(!file.eof()){
       file >> user.user_number >> user.user_pin >> user.fname >> user.lname >> user.user_type;
-      // std::cout << "Fetched : " << user.user_number << " With id : " << user.user_number << std::endl;
-      if(user.user_number == update_user.user_number){
+      if(user.user_number == user_id){
         user.user_number = update_user.user_number;
         user.user_pin = update_user.user_pin;
         user.fname = update_user.fname;
@@ -125,15 +130,14 @@ public:
     return true;
   }
 
-   bool update(Course update_course){
-    update_course.file_name = "files/studentfiles/" + course.user_number + ".txt";
+   bool update(std::string user_id,Course update_course){
+    update_course.file_name = "files/studentfiles/" + update_course.user_number + ".txt";
     //lines to update the course in the file
     file.open(update_course.file_name.c_str());
     temp_file.open("files/studentfiles/temp.txt",std::ios::app|std::ios::out);
     while(!file.eof()){
-      file >> course.course_code >> course.course_title >> course.credit;
-      // std::cout << "Fetched : " << user.user_number << " With id : " << user.user_number << std::endl;
-      if(course.course_code == update_course.course_code){
+      file >> course.course_code >> course.course_title >> course.credit >> course.grade >> course.gradept;
+      if(course.course_code == user_id){
         course.course_code = update_course.course_code;
         course.course_title = update_course.course_title;
         course.credit = update_course.credit;
@@ -147,17 +151,13 @@ public:
     return true;
   }
 
-  //delete(remove user) methods
   bool delete_user(std::string user_id){
     //lines to delete the user
     file.open("files/users.txt");
     temp_file.open("files/temp.txt",std::ios::app|std::ios::out);
     while(!file.eof()){
       file >> user.user_number >> user.user_pin >> user.fname >> user.lname >> user.user_type;
-      // std::cout << "Fetched : " << user.user_number << " With id : " << user.user_number << std::endl;
       if(user.user_number == user_id){
-        //if it passes for the test it will skip the rest if the lines in the loop
-        //hence not writing the user we want to delete to the file
         continue;
       }
       temp_file << user.user_number << " " << user.user_pin << " " << user.fname << " " << user.lname << " " << user.user_type << std::endl;
@@ -176,10 +176,7 @@ public:
     temp_file.open("files/studentfiles/temp.txt",std::ios::app|std::ios::out);
     while(!file.eof()){
       file >> course.course_code >> course.course_title >> course.credit >> course.grade >> course.gradept;
-      // std::cout << "Fetched : " << user.user_number << " With id : " << user.user_number << std::endl;
       if(course.course_code == course_code){
-        //if it passes for the test it will skip the rest if the lines in the loop
-        //hence not writing the user we want to delete to the file
         continue;
       }
       temp_file << course.course_code << " " << course.course_title << " " << course.credit << " " << course.grade << " " << course.gradept << std::endl;
@@ -188,12 +185,25 @@ public:
     temp_file.close();
     remove(id.c_str());
     rename("files/studentfiles/temp.txt",id.c_str());
+
+    std::string course_name = "files/courses/" + course_code + ".txt";
+    course_file.open(course_name.c_str());
+    temp_file.open("files/courses/temp.txt",std::ios::app|std::ios::out);
+    while(!course_file.eof()){
+      course_file >> course.user_number;
+      if(course.user_number == user_ID){
+        continue;
+      }
+      temp_file << course.user_number << std::endl;
+    }
+    course_file.close();
+    temp_file.close();
+    remove(course_name.c_str());
+    rename("files/courses/temp.txt",course_name.c_str());
     return true;
   }
 
-  //user authentication method
   bool login(std::string user_id,std::string user_pin){
-    //line to log user into his/her account
     User user;
     this->fetch(user_id,user);
     if(user.user_pin == user_pin){
@@ -203,12 +213,11 @@ public:
   }
 
   void grade_student(Course& update_course){
-    //this->fetch(update_course.course_code,update_course);
     update_course.file_name = "files/studentfiles/" + update_course.user_number + ".txt";
     file.open(update_course.file_name.c_str());
     temp_file.open("files/studentfiles/temp.txt",std::ios::app|std::ios::out);
     while(!file.eof()){
-      file >> course.course_code >> course.raw_score >> course.grade >> course.gradept;
+      file >> course.course_code >> course.course_title >> course.credit >> course.grade >> course.gradept;
       if(course.course_code == update_course.course_code){
         course.raw_score = update_course.raw_score;
         if(course.raw_score >= 80 && course.raw_score <=100){
@@ -240,16 +249,9 @@ public:
     }
     file.close();
     temp_file.close();
-    remove(course.file_name.c_str());
-    rename("files/stuentfiles/temp.txt",course.file_name.c_str());
+    remove(update_course.file_name.c_str());
+    rename("files/studentfiles/temp.txt",update_course.file_name.c_str());
     std::cout << "Student has been graded successfully .." << std::endl;
-  }
-
-  void give_assignment(std::string course_code,std::string assignment){
-    file.open("files/assignment.txt",std::ios::app|std::ios::out);
-    file << course_code << " " << assignment << std::endl;
-    file.close();
-    std::cout << "Assignment uploaded!" << std::endl;
   }
 
   void print(std::string user_id){
@@ -257,13 +259,11 @@ public:
     file.open(filename,std::ios::in);
     while(!file.eof()){
       getline(file,filename);
-      //pardon me I am feeling lazy to create a new variable so I am killing it all here
       std::cout << filename << std::endl;
     }
+    file.close();
   }
 
 };
-
-
 
 #endif
--- Project/p_10665054.cpp
+++ Project/p_10665054.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char const *argv[]) {
 
 int choice,admin_opt,staff_opt,stud_opt;
-string admin_no,admin_pin,staff_no,staff_pin,stud_no,stud_pin;
+string admin_no,admin_pin,staff_no,staff_pin,stud_no,stud_pin,assignment;
 User one,user;
 Course course,edit_course;
 bool check;
@@ -33,10 +33,11 @@ Database dbase;
         system("cls");
         adminPage:
           cout << "--------------------ADMIN----------------------\n\n";
-          cout <<"1.Add user\n2.Edit user\n3.Delete user\n4.Log out\n";
+          cout <<"1.Add user\n2.View user\n3.Edit user\n4.Delete user\n5.Log out\n";
           cin >> admin_opt;
         switch(admin_opt){
             case 1:
+                system("cls");
                 cout << "Enter user number : ";
                 cin >> one.user_number;
                 stud_no = one.user_number;
@@ -50,13 +51,21 @@ Database dbase;
                 cout << "Enter user type (admin/staff/student) : ";
                 cin >> one.user_type;
               dbase.create(one);
+              system("pause");
               system("cls");
               goto adminPage;
             break;
             case 2:
+              system("cls");
+              dbase.view_users();
+              system("pause");
+              system("cls");
+              goto adminPage;
+            break;
+            case 3:
+              system("cls");
               cout << "Enter user number of person to edit: ";
               cin >> stud_no;
-              dbase.fetch(stud_no,one);
               cout << "Enter new user number : ";
               cin >> user.user_number;
               cout << "Enter new user pin : ";
@@ -67,31 +76,37 @@ Database dbase;
               cin >> user.lname;
               cout << "Enter new user type (admin/staff/student) : ";
               cin >> user.user_type;
-              if(dbase.update(user)){
+              if(dbase.update(stud_no,user)){
                 cout << "User edited successfully .. " << endl;
               }
-              system("cls");
-              goto adminPage;
-            break;
-            case 3:
-              cout << "Enter user number of person to delete: ";
-              cin >> stud_no;
-              if(dbase.delete_user(stud_no)){
-                cout << "User deleted successfully" << endl;
-              }
+              system("pause");
               system("cls");
               goto adminPage;
             break;
             case 4:
               system("cls");
+              cout << "Enter user number of person to delete: ";
+              cin >> stud_no;
+              if(dbase.delete_user(stud_no)){
+                cout << "User deleted successfully" << endl;
+              }
+              system("pause");
+              system("cls");
+              goto adminPage;
+            break;
+            case 5:
+              system("cls");
               goto start;
             break;
             default:
               cout << "Invalid input\n";
+              system("pause");
+              system("cls");
               goto adminPage;
       }
       }else{
         cout << "Invalid admin number or pin\n";
+        system("pause");
         system("cls");
         goto start1;
       }
@@ -108,7 +123,7 @@ Database dbase;
         system("cls");
         staffPage:
           cout << "------------------------STAFF---------------------\n\n";
-          cout << "1.Grade student\n2.Give assignment\n3.Log out\n";
+          cout << "1.Grade student\n2.View student list\n3.Log out\n";
           cin >> staff_opt;
 
         switch(staff_opt){
@@ -126,19 +141,21 @@ Database dbase;
             cin >> edit_course.raw_score;
 
             dbase.grade_student(edit_course);
+            system("pause");
             system("cls");
             goto staffPage;
 
           break;
           case 2:
-            cout << "Enter course code : ";
+            system("cls");
+            cout << "Enter the course code to view list of students assigned to that course : ";
             cin >> admin_no;
-            cout << "Enter assignment : ";
-            getline(cin,admin_pin);
-            dbase.give_assignment(admin_no,admin_pin);
+            if(dbase.view_student(admin_no) == 1){
+              cout << "No student has registered for this course yet" << endl;
+            }
+            system("pause");
             system("cls");
             goto staffPage;
-
           break;
           case 3:
             system("cls");
@@ -146,10 +163,13 @@ Database dbase;
           break;
           default:
             cout << "Invalid input\n";
+            system("cls");
             goto staffPage;
         }
         }else{
           cout << "Invalid staff number or pin\n";
+          system("pause");
+          system("cls");
           goto start2;
       }
     break;
@@ -165,10 +185,11 @@ Database dbase;
         system("cls");
         studPage:
           cout << "------------------------STUDENT---------------------\n\n";
-          cout << "1.Add courses\n2.View courses\n3.Edit courses\n4.Delete courses\n5.Academic Record\n6.Log out\n";
+          cout << "1.Add course\n2.View course(s)\n3.Edit course(s)\n4.Delete course(s)\n5.Academic Record\n6.Log out\n";
           cin >> stud_opt;
         switch(stud_opt){
           case 1:
+            system("cls");
             course.user_number = stud_no;
             cout << "Enter course code : ";
             cin >> course.course_code;
@@ -184,7 +205,8 @@ Database dbase;
             goto studPage;
           break;
           case 2:
-            if(dbase.view(stud_no) == 1){
+            system("cls");
+            if(dbase.view_course(stud_no) == 1){
               cout << "you have not registered for any courses yet" << endl;
             }
             system("pause");
@@ -192,33 +214,38 @@ Database dbase;
             goto studPage;
           break;
           case 3:
-            course.user_number = stud_no;
+            system("cls");
             cout << "Enter course code of course to edit: ";
             cin >> admin_no;
-            dbase.fetch(admin_no,course);
+            edit_course.user_number = stud_no;
             cout << "Enter new course code : ";
             cin >> edit_course.course_code;
             cout << "Enter new course title : ";
             cin >> edit_course.course_title;
             cout << "Enter new course credit hours : ";
-            cin >> course.credit;
-            if(dbase.update(edit_course)){
+            cin >> edit_course.credit;
+            if(dbase.update(admin_no,edit_course)){
               cout << "Course edited successfully .. " << endl;
             }
+            system("pause");
             system("cls");
             goto studPage;
           break;
           case 4:
+            system("cls");
             cout << "Enter course code of course to delete: ";
             cin >> admin_no;
             if(dbase.delete_course(stud_no,admin_no)){
               cout << "Course deleted successfully" << endl;
             }
+            system("pause");
             system("cls");
             goto studPage;
           break;
           case 5:
+            system("cls");
             dbase.print(stud_no);
+            system("pause");
             system("cls");
             goto studPage;
           break;
@@ -227,17 +254,23 @@ Database dbase;
             goto start;
           break;
           default:
-            cout << "Invalid input\n";
+            cout << "Invalid input";
+            system("pause");
+            system("cls");
             goto studPage;
         }
         }else{
-          cout << "Invalid student number or pin\n";
+          cout << "Invalid student number or pin";
+          system("pause");
+          system("cls");
           goto start3;
 
       }
     break;
     default:
       cout << "Invalid input";
+      system("pause");
+      system("cls");
       goto start;
   }
 
